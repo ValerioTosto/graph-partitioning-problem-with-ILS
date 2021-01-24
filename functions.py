9b5c638cbdc0a8337cd0f9a6da37dad4330f4e2d
ValerioTosto <valeriotosto@libero.it>
2021-01-24 17:53:26 +0100

    List replacement with graphs

--- functions.py
+++ functions.py
@@ -3,26 +3,29 @@ import matplotlib.pyplot as plt
 import random
 
 # Draw Partitions
-def draw_graph(G, pos, P1, P2, title = ''):
+def draw_graph(G, pos, title = ''):
+    (P1, P2) = get_partitions(G)
+    #(P1, P2) = (v1, v2)
     nx.draw_networkx_nodes(G, pos, nodelist=P1, node_color='#de3c19')
     nx.draw_networkx_nodes(G, pos, nodelist=P2)
     nx.draw_networkx_labels(G, pos)
     nx.draw_networkx_edges(G, pos, arrows=False)
-    nx.draw_networkx_edges(G, pos, edgelist=generate_edge_list(G, P1, P2), width=3, alpha=0.5, edge_color='r')
-    plt.title(title + ' cut-size - ' + str(cut_size_value(G, P1, P2)))
+    nx.draw_networkx_edges(G, pos, edgelist=generate_cut_edge_list(G), width=3, alpha=0.5, edge_color='r')
+    print(title, ' cut-size - ', cut_size_value(G))
+    plt.title(title + ' cut-size - ' + str(cut_size_value(G)))
     plt.show()
 
 # Generate edge_list from partitions
-def generate_edge_list(G, P1, P2):
+def generate_cut_edge_list(G):
     edge_list = []
-    for x in P1:
-        for y in P2:
-            if G.has_edge(x, y) or G.has_edge(y, x):
-                edge_list.extend([(x,y)])
+    for u in G.nodes():
+        for v in G.neighbors(u):
+            if G.nodes[v]['partition'] != G.nodes[u]['partition']:
+                edge_list.extend([(u,v)])
     return edge_list
 
 # Calculate Partitions Cut Size
-def cut_size_value(G, P1, P2):
+def cut_size_value_old(G, P1, P2):
     cut_size = 0
     for x in P1:
         for y in P2:
@@ -30,30 +33,258 @@ def cut_size_value(G, P1, P2):
                 cut_size += 1
     return cut_size
 
+# Calculate Partitions Cut Size
+def cut_size_value(G):
+    return len(generate_cut_edge_list(G))
+
 # Print Graph info
 def graph_info(K, n, G, P1, P2):
-    print('K - ' + str(K))
-    print('# cycle - ' + str(n))
-    print('Graph nodes - ' + str(G.number_of_nodes()))
-    print('Graph edges - ' + str(len(G.edges())))
-    print('# Partition 1 - ' + str(len(P1)))
-    print('# Partition 2 - ' + str(len(P2)))
+    print('K - ', K)
+    print('# cycle - ', n)
+    print('Graph nodes - ', G.number_of_nodes())
+    print('Graph edges - ', len(G.edges()))
+    print('# Partition 1 - ', len(P1))
+    print('# Partition 2 - ', len(P2))
 
 # Generate random partitions
 def generate_solution(G):
     nodes = G.number_of_nodes()
     nodes_list = list(G.nodes)
-    V1 = []
-    V2 = []
+    #V1 = []
+    #V2 = []
     indexes = random.sample(range(0, nodes), round(nodes/2))
     for i in indexes:
-        V1.append(nodes_list[i])
-    V2.extend(set(nodes_list) - set(V1))
+        #V1.append(nodes_list[i])
+        G.nodes[nodes_list[i]]['partition'] = 1
+    #V2.extend(set(nodes_list) - set(V1))
+
+    return get_partitions(G)
+
+
+# Generate random partitions
+def get_partitions(G):
+    nodes_list = list(G.nodes)
+    V1 = []
+    V2 = []
+    for u in nodes_list:
+        if G.nodes[u]['partition'] == 1:
+            V1.append(u)
+        else:
+            V2.append(u)
 
     return (V1, V2)
 
+# Generate neighborhood
+def generate_neighborhood(G, S):
+    V1, V2 = S[0].copy(), S[1].copy()
+
+    nodes = G.number_of_nodes()
+    nodes_list = list(G.nodes)
+
+    max_neighbors_v1 = 0
+    max_neighbors_v1_index = 0
+    max_neighbors_v2 = 0
+    max_neighbors_v2_index = 0
+
+    for i,x in enumerate(V1):
+        count = 0
+        for y in G.neighbors(x):
+            if y in V2:
+                count += 1
+        if count > max_neighbors_v1:
+            max_neighbors_v1 = count
+            max_neighbors_v1_index = i
+
+    for i,x in enumerate(V2):
+        count = 0
+        for y in G.neighbors(x):
+            if y in V1:
+                count += 1
+        if count > max_neighbors_v2:
+            max_neighbors_v2 = count
+            max_neighbors_v2_index = i
+    
+    V2.append(V1.pop(max_neighbors_v1_index))
+    V1.append(V2.pop(max_neighbors_v2_index))
+
+    Sn = [V1, V2]
+    return Sn
+
+def generate_neighborhood2(G, S):
+    V1, V2 = S[0].copy(), S[1].copy()
+
+    nodes = G.number_of_nodes()
+    nodes_list = list(G.nodes)
+
+    min_neighbors_v1 = nodes
+    min_neighbors_v1_index = 0
+    min_neighbors_v2 = nodes
+    min_neighbors_v2_index = 0
+
+    for i,x in enumerate(V1):
+        count = 0
+        for y in G.neighbors(x):
+            if y in V2:
+                count += 1
+        if count < min_neighbors_v1:
+            min_neighbors_v1 = count
+            min_neighbors_v1_index = i
+
+    for i,x in enumerate(V2):
+        count = 0
+        for y in G.neighbors(x):
+            if y in V1:
+                count += 1
+        if count < min_neighbors_v2:
+            min_neighbors_v2 = count
+            min_neighbors_v2_index = i
+    
+    V2.append(V1.pop(min_neighbors_v1_index))
+    V1.append(V2.pop(min_neighbors_v2_index))
+
+    Sn = [V1, V2]
+    return Sn
+
+def generate_multiple_neighborhood(G, S):
+    V1, V2 = S[0].copy(), S[1].copy()
+    Smn = []
+    nodes = G.number_of_nodes()
+    nodes_list = list(G.nodes)
+
+    max_cycle = len(V1)
+    if len(V1) != len(V2):
+        max_cycle = min(len(V1), len(V2))
+    
+    for i in range(max_cycle):
+        V2.append(V1.pop(i))
+        V1.append(V2.pop(i))
+        Sn = [V1.copy(), V2.copy()]
+        Smn.append(Sn)
+    
+    return Smn
+
+def generate_multiple_neighborhood2(G, S):
+    V1, V2 = S[0].copy(), S[1].copy()
+    Smn = []
+    nodes = G.number_of_nodes()
+    nodes_list = list(G.nodes)
+
+    max_ext_neighborhood_v1 = 0
+    max_int_neighborhood_v1 = len(G.edges())
+    max_neighborhood_v1_index = 0
+    max_ext_neighborhood_v2 = 0
+    max_int_neighborhood_v2 = len(G.edges())
+    max_neighborhood_v2_index = 0
+
+    for i,x in enumerate(V1):
+        ext_neighborhood = 0
+        int_neighborhood = 0
+        for j,y in enumerate(G.neighbors(x)):
+            if y in V2:
+                ext_neighborhood += 1
+            else:
+                int_neighborhood += 1
+
+        if ext_neighborhood > max_ext_neighborhood_v1 and int_neighborhood < max_int_neighborhood_v1:
+            max_ext_neighborhood_v1 = ext_neighborhood
+            max_int_neighborhood_v1 = int_neighborhood
+            max_neighborhood_v1_index = i
+
+    for i,x in enumerate(V2):
+        ext_neighborhood = 0
+        int_neighborhood = 0
+        for j,y in enumerate(G.neighbors(x)):
+            if y in V1:
+                ext_neighborhood += 1
+            else:
+                int_neighborhood += 1
+
+        if ext_neighborhood > max_ext_neighborhood_v2 and int_neighborhood < max_int_neighborhood_v2:
+            max_ext_neighborhood_v2 = ext_neighborhood
+            max_ext_neighborhood_v2 = int_neighborhood
+            max_neighborhood_v2_index = i
+    
+    V2.append(V1.pop(max_neighborhood_v1_index))
+    V1.append(V2.pop(max_neighborhood_v2_index))
+    Sn = [V1.copy(), V2.copy()]
+    Smn.append(Sn)
+    
+    return Smn
+
+def generate_multiple_neighborhood_graph(G):
+    Smn = []
+
+    max_ext_neighborhood = 0
+    min_int_neighborhood = len(G.edges())
+    node = 0 #node with max external neighborhood and min internal neighborhood
+
+    for u in G.nodes():
+        ext_neighborhood = 0
+        int_neighborhood = 0
+        for v in G.neighbors(u):
+            if G.nodes[v]['partition'] != G.nodes[u]['partition']:
+                ext_neighborhood += 1
+            else:
+                int_neighborhood += 1
+
+            if ext_neighborhood > max_ext_neighborhood:
+                    max_ext_neighborhood = ext_neighborhood
+                    min_int_neighborhood = int_neighborhood
+                    node = v
+            elif ext_neighborhood == max_ext_neighborhood:
+                if int_neighborhood < min_int_neighborhood:
+                    min_int_neighborhood = int_neighborhood
+                    node = v
+    
+    for v in G.neighbors(node):
+        #print(node, ' - ', v, ' - ', len(list(G.neighbors(node))))
+        if G.nodes[node]['partition'] != G.nodes[v]['partition']:
+            ns_G = G.copy()
+            ns_G.nodes[node]['partition'], ns_G.nodes[v]['partition'] = G.nodes[v]['partition'], G.nodes[node]['partition']
+            #print (node, ' - ', G.nodes[node]['partition'], '->', ns_G.nodes[node]['partition'])
+            #print('cutsize=', cut_size_value(ns_G))
+            Smn.append(ns_G.copy())
+        
+    return Smn
+
+def best_neighborhood_old(G, Smn):
+    index = 0
+    bcsv = len(G.edges())
+    for i,Sn in enumerate(Smn):
+        csv = cut_size_value(G, Sn[0], Sn[1])
+        if csv < bcsv:
+            bcsv = csv
+            index = i
+    
+    return Smn[index]
+
+
+def best_neighborhood(Smn):
+    index = 0
+    bcsv = len(Smn[0].edges())
+    #bcsv=99999999999999999
+    for i,Sn in enumerate(Smn):
+        csv = cut_size_value(Sn)
+        if csv < bcsv:
+            bcsv = csv
+            index = i
+    #print('bestcutsize=', cut_size_value(Smn[index]))
+    return Smn[index]
 
 # Make perturb using swap
-def swap(P1, P2):
-    i = random.randint(0, len(P1)-1)
-    P1[i], P2[i] = P2[i], P1[i]
+def swap_old(P1, P2):
+    for x in range(round(len(P1)/5)): #swap only 5%
+        i = random.randint(0, len(P1)-1)
+        P1[i], P2[i] = P2[i], P1[i]
+
+def swap(G, P1, P2):
+    ns_G = G.copy()
+    for x in range(round(len(P1)/5)): #swap only 5%
+        i = random.randint(0, len(P1)-1)
+        #P1[i], P2[i] = P2[i], P1[i]
+        ns_G.nodes[P1[i]]['partition'], ns_G.nodes[P2[i]]['partition'] = G.nodes[P2[i]]['partition'], G.nodes[P1[i]]['partition']
+    return ns_G
+
+def local_search(G, S):
+    V1, V2 = S[0].copy(), S[1].copy()
+    # do something
--- main.py
+++ main.py
@@ -11,7 +11,9 @@ G = nx.Graph()
 file = open("edges.txt", "r")
 for row in file:
     x = row.replace("\n", "").split(" ")
-    G.add_edges_from([(int(x[0]), int(x[1]))])
+    G.add_node(int(x[0]), partition = 0)
+    G.add_node(int(x[1]), partition = 0)
+    G.add_edge(int(x[0]), int(x[1]))
 
 # Draw initial graph
 pos = nx.spring_layout(G)
@@ -24,7 +26,7 @@ plt.show()
 K = 2
 
 # Define cycling number
-n = 50000
+n = 5000
     
 # Define initial solution S
 (V1, V2) = fun.generate_solution(G)
@@ -33,26 +35,54 @@ S = [V1, V2]
 # Print initial graph info
 fun.graph_info(K, n, G, V1, V2)
 
-s_cut_size = fun.cut_size_value(G, V1, V2)
+s_cut_size = fun.cut_size_value(G)
+#s_cut_size = fun.cut_size_value(G, V1, V2)
 #print("cut size: " + str(s_cut_size))
 
 
 # Draw Original Partitions
-fun.draw_graph(G, pos, V1, V2, 'Original Partitions')
+fun.draw_graph(G, pos, 'Original Partitions')
     
 
 or_V1 = V1.copy()
 or_V2 = V2.copy()
 
+print("V1 - Before editing", V1)
+print("V2 - Before editing", V2)
 
+#Sn = fun.generate_neighborhood(G, S)
+#
+#print("V1 - After editing", V1)
+#print("V2 - After editing", V2)
+#print("Sn[0] - After editing", Sn[0])
+#print("Sn[1] - After editing", Sn[1])
+Sn = G
 for x in range(n):
     if ( (x/n * 100) % 10 == 0):
         print('loop percentage: ' + str(x/n * 100) + ' %')
+
     r_V1 = V1.copy()
     r_V2 = V2.copy()
+
+    #print("pre neigh - " + str(fun.cut_size_value(Sn)))
+    #Sn = fun.generate_neighborhood(G, [r_V1, r_V2])
     
-    fun.swap(r_V1, r_V2)
-    r_s_cut_size = fun.cut_size_value(G, r_V1, r_V2)
+    Smn = fun.generate_multiple_neighborhood_graph(Sn)
+    #Smn = fun.generate_multiple_neighborhood2(G, [r_V1, r_V2])
+    #print(len(Smn))
+    #Sn = fun.best_neighborhood(G, Smn)
+    Sn = fun.best_neighborhood(Smn)
+
+    r_V1, r_V2 = fun.get_partitions(Sn)
+    #r_V1 = Sn[0].copy()
+    #r_V2 = Sn[1].copy()
+    #print("post neigh - " + str(fun.cut_size_value(Sn)))
+
+    #fun.swap(r_V1, r_V2)
+    Sn = fun.swap(Sn, r_V1, r_V2)
+    r_s_cut_size = fun.cut_size_value(Sn)
+    #r_s_cut_size = fun.cut_size_value(G, r_V1, r_V2)
+    #print("post swap - " + str(fun.cut_size_value(Sn)))
     #fun.draw_graph(G, pos, r_V1, r_V2, 'temp partitions')
     
     if r_s_cut_size < s_cut_size:
@@ -61,5 +91,10 @@ for x in range(n):
         s_cut_size = r_s_cut_size
     
 
+print("V1 - After editing", V1)
+print("V2 - After editing", V2)
+#print("Sn[0] - After editing", Sn[0])
+#print("Sn[1] - After editing", Sn[1])
+
 # Draw Final Partition Graph
-fun.draw_graph(G, pos, V1, V2, 'Final Partitions')
+fun.draw_graph(Sn, pos, 'Final Partitions')
